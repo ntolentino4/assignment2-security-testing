a3bf80186e2ba7dccbf52d3b2e5da00ae7acae49
Neil Tolentino <ntolentino4@academic.rrc.ca>
2025-11-22 00:59:24 -0600

    Updated Sample code Assignment 1 java file.

--- assignment1_code_sample_3.java
+++ assignment1_code_sample_3.java
@@ -11,7 +11,7 @@ public class VulnerableApp {
 
     private static final String DB_URL = "jdbc:mysql://mydatabase.com/mydb";
     private static final String DB_USER = "admin";
-    private static final String DB_PASSWORD = "secret456";
+    private static final String DB_PASSWORD = "secret789";
 
     public static String getUserInput() {
         Scanner scanner = new Scanner(System.in);
